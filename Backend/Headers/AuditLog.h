2d139ca67170cce9de139118577b6577e99d309d
prakharpower <prakhartrivedi9369@gmail.com>
2026-08-05 19:39:25 +0530

    Audit Logs feature is in progress!

---
+++ Backend/Headers/AuditLog.h
@@ -0,0 +1,47 @@
+#pragma once
+
+#include "crow.h"
+#include <string>
+
+using namespace std;
+
+enum class AuditEvent
+{
+    LOGIN_SUCCESS,
+    LOGIN_FAILED,
+
+    SIGNUP_SUCCESS,
+    SIGNUP_FAILED,
+
+    OTP_SENT,
+    OTP_VERIFIED,
+    OTP_FAILED,
+
+    PASSWORD_RESET,
+
+    RATE_LIMIT_BLOCK,
+
+    UNKNOWN
+};
+
+enum class AuditStatus
+{
+    SUCCESS,
+    FAILED,
+    BLOCKED,
+    EXPIRED,
+    UNKNOWN
+};
+
+struct AuditLog
+{
+    AuditEvent event;
+    AuditStatus status;
+
+    std::string email;
+    std::string ip_address;
+    std::string timestamp;
+};
+
+std::string toString(AuditEvent event);
+std::string toString(AuditStatus status);
